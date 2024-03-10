73c632f7dcb42890bd581c780ebbaa097bb6a720
K0ndrich <123kondrich@gmail.com>
2024-03-10 10:44:36 +0200

    added top menu + set urls

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -15,14 +15,8 @@ urlpatterns = [
     # name позволяет использовать заданый маршрут в других файлах проекта
     path("", views.index, name="home"),
     path("about/", views.about, name="about"),
-    # int: конвертер преобразовывает значение строки от пользователя url в тип int
-    # car_id название переменной
-    path("cats/<int:cat_id>/", views.categories, name="cats_id"),
-    # slug принимает любое слово в url
-    path("cats/<slug:cat_slug>/", views.categories_by_slug, name="cats"),
-    # принимает число длиной только в 4 символа, котрое состоит из цивт от 0 до 9
-    # имеет приоритет выполение више
-    # re_path(r"^archive/(?P<year>[0-9]{4})/$", views.archive),
-    # используем свой конвертор
-    path("archive/<year4:year>/", views.archive),
+    path("addpage/", views.addpage, name="add_page"),
+    path("contact/", views.contact, name="contact"),
+    path("login/", views.login, name="login"),
+    path("post/<int:post_id>/", views.showpost, name="post"),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -14,21 +14,33 @@ from django.shortcuts import render, redirect
 # reverse записивает в переменую путь с передачей аргументов
 from django.urls import reverse
 
-#  from django.shortcuts import render
+menu = [
+    {"title": "О сайте", "url_name": "about"},
+    {"title": "Добавить Статью", "url_name": "add_page"},
+    {"title": "Обратная связь", "url_name": "contact"},
+    {"title": "Войти", "url_name": "login"},
+]
 
-# render_to_string  позволяет работать с шаблонами (не рекомендуеться)
-from django.template.loader import render_to_string
-# подключение фильтра шаблона как отдельную функцию
-# slugify - конвертирует теккс в тип данных slugy
-from django.template.defaultfilters import slugify
-
-menu = ["О сайте", "Добавить Статью", "Обратная связь", "Войти"]
-
-
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [
+    {
+        "id": 1,
+        "title": "Анджелина Джоли",
+        "content": "Биография Анджелины Джоли",
+        "is_published": True,
+    },
+    {
+        "id": 2,
+        "title": "Марго Робби",
+        "content": "Биография Марго Робби",
+        "is_published": False,
+    },
+    {
+        "id": 3,
+        "title": "Джулия Робертс",
+        "content": "Биография Джулия Робертс",
+        "is_published": True,
+    },
+]
 
 
 # HTTP request - хранить иформацию о текущем запросе от пользователя
@@ -36,14 +48,9 @@ def index(request):
     # t = render_to_string("women/index.html")
     # return HttpResponse(t)
     data = {
-        "title": "главная страница",
+        "title": "Главная Страница",
         "menu": menu,
-        "float": 28.56,
-        "lst": [1, 2.3, True, "abc"],
-        "set": {1, 2, 3, 2, 5},
-        "dict": {"key1": "value_1", "key2": "value_2"},
-        "obj": MyClass(10, 20),
-        "url": slugify("the main page"),
+        "posts": data_db,
     }
     # 3-й параметр ето значения которые подставляем в шаблон , context можна и не указывать
     return render(request, "women/index.html", context=data)
@@ -56,32 +63,21 @@ def about(request):
     return render(request, "women/about.html", data)
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>This id -> {cat_id}</p>")
+def showpost(request, post_id):
+    return HttpResponse(f"Отображение Статьи с ID -> {post_id}")
 
 
-def categories_by_slug(request, cat_slug):
-    # request.GET содержит ключи и значения параметров GET запроса
-    print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>This text -> {cat_slug} </p>")
+def addpage(request):
+    return HttpResponse("Добавление Статьи")
 
 
-def archive(request, year):
-    if year > 2023:
+def contact(request):
+    return HttpResponse("Обратная Связь")
 
-        # Http404 выводит преставление которое мы передали в handler404 в sitewomen/urls.py
-        # raise Http404()
 
-        # reverse записывает путь в переменую, также указывает аргументы для пути URL
-        uri = reverse("cats", args=("music",))
-
-        # перенаправление на страницу по указаному URL , если if возвращает True
-        # permanent=True код состояние 302, а если False код состояние 301 (постоянное перемищение)
-        # return redirect(uri, permanent=False) == HttpResponseRedirect(uri)
-        return redirect(uri, permanent=True)  # == HttpResponsePermanentRedirect(uri)
-
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>This year -> {year}</p>")
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def page_not_found(request, exception):
-    return HttpResponseNotFound("<h1>function --> page_not_found</h1>")
+    return HttpResponseNotFound("<h1>Страница Не Найдена</h1>")
